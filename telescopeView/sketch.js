c8569eb5f82db6b0aa19917af4c890ff2dc59bec
Greg K <gkreisman_cep@qq.com>
2025-05-13 21:58:00 +0800

    add telescopeView

---
+++ telescopeView/sketch.js
@@ -0,0 +1,95 @@
+let fated = false;
+let greg; //variable to hold gurtle
+let population = [];
+let pressed = false; //for de bouncing
+let bg;
+let cnv;
+let capture;
+
+//function preload() {
+// bg = loadImage("bgbj.jpg");
+//}
+function setup() {
+  pixelDensity(1);
+  cnv = createCanvas(windowWidth, windowHeight);
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment",
+      },
+    },
+    //video: {
+    //facingMode: "user"
+    //}
+  };
+  //if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  capture = createCapture(VIDEO);
+  //}else{
+  // false for not mobile device
+  // capture = createCapture(VIDEO)
+  //}
+  //
+  //capture = createCapture(VIDEO)
+  capture.size(width / 5, height / 5);
+  capture.hide();
+  //let cx = (windowWidth - cnv.width) / 2;
+  //let cy = (windowHeight - cnv.height) / 2;
+  //cnv.position(cx, cy);
+  //bg.resize(width, height);
+  angleMode(DEGREES);
+  for (let i = 0; i < 21; i++) {
+    population[i] = new Creature(color(255, 255), random(5, 30));
+  }
+}
+
+function draw() {
+  image(capture, 0, 0, width, height);
+  for (let i = 0; i < population.length; i++) {
+    population[i].show();
+    population[i].squirm(frameCount);
+    population[i].wither(); // forgot to call tis
+    population[i].move();
+  }
+
+  if (!fated) {
+    fill(255, 255, 0);
+    textSize(30);
+    noStroke();
+    text("Press anywhere to choose fate", 10, 50);
+  } else {
+    fill(255, 0, 0);
+    textSize(20);
+    noStroke();
+    text("After result press anywhere reset population", 10, 50);
+  }
+}
+
+function mouseClicked() {
+  let fate = random([instr, instr2]);
+  if (!pressed) {
+    pressed = true;
+    if (!fated) {
+      for (let i = 0; i < population.length; i++) {
+        if (population[i].gene === fate) {
+          population[i].dying = true;
+        }
+        fated = true;
+      }
+    } else {
+      for (let i = 0; i < population.length; i++) {
+        population[i].gene = random([instr, instr2]);
+        population[i].dying = false;
+        population[i].dead = false;
+        population[i].sz = random(5, 30); //reset size
+        population[i].deathClock = random(100, 500); // need to reset death clock too
+      }
+      fated = false;
+    }
+  }
+}
+
+function mouseReleased() {
+  pressed = false;
+}
